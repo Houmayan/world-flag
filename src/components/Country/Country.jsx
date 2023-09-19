5ad08b4a8b1fdfc83e6b380efa8c4d4cdef7843d
houmayan <alichy101@gmail.com>
2023-09-19 23:58:49 +0600

    completed

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-    const {name,cca3,flags} = country;
-    // console.log(country);
+const Country = ({country,handleVisitedCountries,handleVisitedFlags,handleNewVisitedFlags}) => {
+    const {name,cca3,flags,population,area} = country;
+    // console.log(handleNewVisitedFlags);
+    const [visited,setVisited] = useState(false);
+    const handleVisited =()=>{
+        setVisited(!visited);
+    }
     return (
-        <div className='country'>
+        <div className={`country ${ visited && 'isVisited'}`}>
             
             <h3>Name: {name?.common}</h3>
             <p>Official Name: {name?.official}</p>
             <img src={flags.png} alt="" />
             <p>{cca3}</p>
+            <p>Populatiion:{population}</p>
+            <p>Land of area: {area} km<sup>2</sup></p>
+            <br />
+            <button onClick={()=>handleNewVisitedFlags(country.flags.svg)}>Again Added Flag:</button>
+            <button onClick={()=>handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <button onClick={()=>handleVisitedCountries(country)}>Mark as visited</button>
+            <button onClick={handleVisited}>{visited?'Visited':'Not visited'}</button>
+           
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,22 +1,79 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import "../countries/countries.css";
 
 const Countries = () => {
-    const [countries,setCountries] = useState([]);
-    // const [name] = countries;
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all').then(res=>res.json()).then(data=>setCountries(data))
-    },[])
-    return (
-        <div>
-            <h3> No of Countries: {countries.length}</h3>
-           {
-            countries.map(country=><Country key={country.cca3} country={country}></Country>)
-           }
-           
-        </div>
-    );
+  const [countries, setCountries] = useState([]);
+
+  const [visitedCountries,setVisitedCountries] = useState([]);
+
+  const [visitedFlags,setVisitedFlags] = useState([]);
+
+  const [newVisitedFLags,setNewVisitedFlags] = useState([]);
+
+  const handleNewVisitedFlags = (svgFlag)=>{
+    const newSvg = [...newVisitedFLags,svgFlag];
+    setNewVisitedFlags(newSvg);
+  }
+
+
+  const handleVisitedCountries = country=>{
+    // console.log('this is mark as visited');
+    // console.log(country);
+    const newVisitedCountries = [...visitedCountries,country]
+    setVisitedCountries(newVisitedCountries);
+  }
+
+
+
+  const handleVisitedFlags = flags=>{
+    const newVisitedFlags = [...visitedFlags,flags]
+    setVisitedFlags(newVisitedFlags);
+  }
+  
+
+
+  // const [name] = countries;
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h3> No of Countries: {countries.length}</h3>
+      <div>
+        <h3>Again Visited:</h3>
+        {
+          newVisitedFLags.map((newS,idx)=><img key={idx} src={newS}></img>)
+        }
+      </div>
+      <div className="flag-container">
+        <h3>Visited Flags:</h3>
+        {visitedFlags.map((flag,idx)=><img key={idx} src={flag}></img>)}
+      </div>
+      <div>
+        <h3>Visited Countries: {visitedCountries.length}</h3>
+        <ul>
+            {visitedCountries.map((country,idx)=><li  key={idx} >{country.name.common}</li>)}
+        </ul>
+      </div>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country 
+          key={country.cca3}
+          handleVisitedFlags={handleVisitedFlags}
+          handleVisitedCountries={handleVisitedCountries}
+          handleNewVisitedFlags={handleNewVisitedFlags}
+          country={country}>
+
+          </Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
